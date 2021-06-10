f0b37b1ce6b350d8d136e0a7dcbe89d3352a2e47
MBaliver <34187537+MBaliver@users.noreply.github.com>
2021-06-10 12:42:01 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,40 +1,35 @@
 #!/usr/bin/env bash
 # ----------------------------- VARIÁVEIS ----------------------------- #
 
-URL_CITRIX="https://downloads.citrix.com/19434/icaclient_21.4.0.11_amd64.deb?__gda__=1623330506_9261f3e2b4089d9d8a02543914daa6e3"
-URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v2.13.0-beta2/Simplenote-linux-2.13.0-beta2-amd64.deb"
+URL_CITRIX="https://drive.google.com/file/d/1wFSyxPJeV7F4SB8DTEsMf9qhdN8aD8jU/view?usp=sharing"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/Apps"
 
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- REQUISITOS ----------------------------- #
-## Removendo travas eventuais do apt ##
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
 
 ## Atualizando o repositório ##
 sudo apt update -y
 
+## Instalando o Flatpak ##
+sudo apt install flatpak -y
+sudo flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualizando o repositório depois da adição de novos repositórios ##
 sudo apt update -y
 
-
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_CITRIX"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
+wget --no-check-certificate "$URL_CITRIX" -O citrix.deb -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 
 ## Instalando pacotes Flatpak ##
 
-sudo apt install flatpak -y
-sudo flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub -y org.kde.kdenlive
 sudo flatpak install flathub -y com.simplenote.Simplenote 
 sudo flatpak install flathub -y org.glimpse_editor.Glimpse 
